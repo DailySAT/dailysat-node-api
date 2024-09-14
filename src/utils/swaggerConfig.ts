1f5c6d32bf656238d36f1b8d9ebfb65d044b2575
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-09-13 23:49:34 -0400

    added production url in the servers for swagger

--- src/utils/swaggerConfig.ts
+++ src/utils/swaggerConfig.ts
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3001",
+        url: ["http://localhost:3001", "https://api.dailysat.tech"],
       },
     ],
   },
