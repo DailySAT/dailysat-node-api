6a8d378ef0c96c61184a4ba768ad6340987ca6c4
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-09-15 15:42:46 -0400

    fixed imports from previous

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import indexController from '../controllers/index.controller.js'
-import authenticateSession from '../middleware/session.middleware.js'
+import { authenticateSession } from '../middleware/session.middleware.js'
 const router = express.Router()
 
 /**
