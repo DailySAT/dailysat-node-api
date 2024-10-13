d8a98f8f1776604f4161476a66f457bd43df9aae
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-10-12 20:16:42 -0400

    added try catch block

--- src/controllers/question.controller.ts
+++ src/controllers/question.controller.ts
@@ -2,32 +2,41 @@ import { Request, Response } from 'express';
 import { db } from '../utils/db.js';
 import { post } from '../schema.js';
 import { eq, sql } from 'drizzle-orm';
+import handleError from '../libs/handleError.js';
 
 const indexController = {
     getReadingQuestions: async (req: Request, res: Response) => {
-        const { type } = req.query
+        try {
+            const { type } = req.query;
 
-        // Getting question based on the speific type of reading problem requested
+            // Validate the type query parameter
+            if (
+                type !== "comprehension" &&
+                type !== "analysis" &&
+                type !== "synthesis" &&
+                type !== "evaluation"
+            ) {
+                return res.status(400).json({ message: "Invalid type specified." });
+            }
 
-        // Making sure the type is valid, or else giving a bad response
-        
-        if (type !== "comprehension" && type !== "analysis" && type !== "synthesis" && type !== "evaluation") {
-            return res.status(400).json({ message: "Invalid type specified." });
-        }        
-        
-        const randomQuestion = await db
-        .select()
-        .from(post)
-        .where(eq(post.topicID, type as string))
-        .orderBy(sql`RANDOM()`)
-        .limit(1)
-        .execute(); // Execute the query
-        
-        res.json({
-            randomQuestion,
-            message: "Your random question for " + type 
-        })
+            // Fetch a random question based on the provided type
+            const randomQuestion = await db
+                .select()
+                .from(post)
+                .where(eq(post.topicID, type as string))
+                .orderBy(sql`RANDOM()`)
+                .limit(1)
+                .execute();
+
+            // Return the random question
+            return res.json({
+                randomQuestion,
+                message: "Your random question for " + type
+            });
+        } catch (error) {
+            handleError(res, error);
+        }
     }
 };
 
-export default indexController
+export default indexController;
