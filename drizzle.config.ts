7a2820b94a098a809d6d7a0ac99e8d433314d8f7
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-10-12 20:13:45 -0400

    added env example + fixed type error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   schema: "./src/schema.ts",
   out: "./drizzle",
   dbCredentials: {
-    url: process.env.DATABASE_URL
+    url: process.env.DATABASE_URL as string
   },
   
 });
