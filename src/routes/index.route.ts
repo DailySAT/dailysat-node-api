1d30b893e7f029b9f5367a51e519115c641f62dc
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-10-29 21:07:38 -0400

    removed swagger comments on the router files, looks super messy

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -3,34 +3,6 @@ import indexController from '../controllers/index.controller.js'
 import { authenticateSession } from '../middleware/session.middleware.js'
 const router = express.Router()
 
-/**
- * @swagger
- * /:
- *   get:
- *     summary: The default endpoint for when API is first accessed
- *     responses:
- *       200:
- *         description: A JSON object with different information about DailySAT backend
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 name:
- *                   type: string
- *                   example: DailySAT API
- *                 author:
- *                   type: string
- *                   example: DailySAT Engineering Team
- *                 date_created:
- *                   type: string
- *                   example: July 28 2024
- *                 message:
- *                   type: string
- *                   example: Let's change the world 🚀
- */
-
- 
 
 router.get("/", authenticateSession, indexController.get)
 
