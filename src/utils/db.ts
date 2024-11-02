b3f4b17645e91f9c1eb4e1397f206b156c32e203
Hemit Patel <patelvhemit@gmail.com>
2024-11-02 15:09:51 -0400

    Update db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -12,7 +12,7 @@ import * as schema from '../schema.js';
 // Using pg's connection pooling so the API does not have to keep creating new connections to the db instance (adds to performance)
 
 export const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL as string
 })
 
 // { schema } is used for relational queries
